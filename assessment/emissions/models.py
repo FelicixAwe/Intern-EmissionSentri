5e01af681cec9b853eba461ff0762dd88a414a54
FelicixAwe <yiquxiao@gmail.com>
2023-10-26 21:52:51 -0700

    modified model fields

--- assessment/emissions/models.py
+++ assessment/emissions/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Emission(models.Model):
     date_created = models.DateField(auto_now_add=True)
     accounting_period = models.CharField(max_length=100)
-    scope2 = models.IntegerField(null=True)
-    scope3 = models.IntegerField(null=True)
-    scope1 = models.IntegerField(null=True)
+    scope2 = models.IntegerField(null=True, blank=True)
+    scope3 = models.IntegerField(null=True, blank=True)
+    scope1 = models.IntegerField(null=True, blank=True)
